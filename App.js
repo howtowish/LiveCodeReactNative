dfe9e67b4ee6356d66d4ec5b5bcd146d20551c65
NewBie\NewBie <luongvanthong312@gmail.com>
2017-05-09 17:29:33 +0700

    init

---
+++ App.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import App from "./src/App.js"
+export default App;
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,Text
+} from "react-native";
+
+class App extends React.Component{
+  render(){
+    return(
+      <View style={{flex:1,backgroundColor:"yellow"}}>
+
+      </View>
+    )
+  }
+}
+export default App
